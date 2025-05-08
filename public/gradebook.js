9c536c344cf0a5687fa211c11e0adcee976b1b77
hbowen245 <hzbowen@my.waketech.edu>
2025-05-07 21:40:16 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -53,5 +53,4 @@ function populateGradebook(data) {
 // TODO REMOVE THIS
 // Call the stubs to demonstrate the workflow
 const gradeData = fetchGradeData();
-populateGradebook(gradeData);
 // END REMOVE
